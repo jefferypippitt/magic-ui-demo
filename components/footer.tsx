f8dc3122f25e37924c8493040e18620480620bd6
jefferypippitt <jefferyspippitt@gmail.com>
2024-06-21 18:23:42 -0500

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto text-center text-zinc-200 py-5 px-7">
+    <footer className="mt-auto text-center text-zinc-500 py-5 px-7">
       <small>&copy; 2024 Jeffery Pippitt. All rights reserved.</small>
     </footer>
   );
